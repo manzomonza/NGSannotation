2e1a06f90a25ca1c6928f20128a8e9ffe39bdcfc
manzomonza <massimiliano.manzo@posteo.ch>
2022-04-07 17:02:05 +0200

    Removed Splice Annot call in read_rename_select_precision_snv

--- R/WatchdogTables_Pipelines.R
+++ R/WatchdogTables_Pipelines.R
@@ -3,7 +3,7 @@
 
 #' ## Output Tables pipelines
 #'
-#' @param snvindel_path 
+#' @param snvindel_path
 #'
 #' @return
 #' @export
@@ -12,20 +12,19 @@
 precisionPipeline_TableOutput <- function(snvindel_path){
   cnv_filepath =  paste0(dirname(snvindel_path),"/Cnv.tsv")
   snv_filepath =  paste0(dirname(snvindel_path),"/Snvindel.tsv")
-  
+
   print(cnv_filepath)
   print(snv_filepath)
   cnv = readIn(cnv_filepath)
-  snv = read_rename_select_precision_snv(snv_filepath)
+  snv = read_rename_select_precision_snv(snv_filepath) %>% splicesite_Annot()
   output_tables <- make_output_tables_precision(snv_indel = snv, cnv = cnv)
   create_output_files(list_of_tables = output_tables,
                       filepath = snvindel_path)
 }
-
 #' Pipeline from intro file to Watchdog output files.
 #' These files can be used for further downstream/alternative website processing.
 #'
-#' @param snv_filepath 
+#' @param snv_filepath
 #'
 #' @return
 #' @export
