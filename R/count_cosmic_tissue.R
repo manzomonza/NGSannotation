09a454d4d5d065dcd4d22b2704ae281d8e2336fb
manzomonza <massimiliano.manzo@posteo.ch>
2022-07-11 09:14:16 +0200

    Add cosmic count wrappers

--- R/count_cosmic_tissue.R
+++ R/count_cosmic_tissue.R
@@ -18,17 +18,18 @@ counter_cosmic_variant_tissue <- function(gene, change, sql_con_tbl){
     return(cosmic_results)
   }
   gene = paste0("%", gene, "%")
+  orig_change = change
   change = paste0("%", change, "%")
 
   ##
   variants_per_tissue = sql_con_tbl %>%
     dplyr::filter(gene_name %LIKE% gene)
 
-  if(grepl("c.", change)){
+  if(grepl("^c.", orig_change)){
   variants_per_tissue = variants_per_tissue %>%
     dplyr::filter(mutation_cds %LIKE% change) %>%
     dplyr::collect()
-  }else if(grepl("p.", change)){
+  }else if(grepl("^p.", orig_change)){
     variants_per_tissue = variants_per_tissue %>%
       dplyr::filter(mutation_aa %LIKE% change) %>%
       dplyr::collect()
@@ -43,9 +44,54 @@ counter_cosmic_variant_tissue <- function(gene, change, sql_con_tbl){
   if(COSMIC_n_total == 0){
     COSMIC_n_tissue = NA
   }else{
-    COSMIC_n_tissue = paste0(count_variants_per_tissue$primary_site, " (",count_variants_per_tissue$n, ")")
+    COSMIC_n_tissue = paste0(count_variants_per_tissue$primary_site, " (", count_variants_per_tissue$n, ")")
   }
   cosmic_results = list(COSMIC_n_tissue = COSMIC_n_tissue,
                         COSMIC_n_total = COSMIC_n_total)
   return(cosmic_results)
 }
+
+#' Wrapper function for COSMIC tissue count
+#'
+#' @param snv_table Unedited watchdog prep_snv table
+#'
+#' @return snv_table with two additional columns: COSMIC_n_total and COSMIC_n_tissue
+#' @export
+#'
+#' @examples
+#' prep_snv.txt
+cosmic_counter_wrapper <- function(snv_table){
+  snv_table = snv_table %>% tibble::rowid_to_column()
+  snv_table$COSMIC_n_total = NA
+  snv_table$COSMIC_n_tissue = NA
+
+  cosmic_count_results = lapply(snv_table$rowid, function(x)  cosmic_counter_per_table_element(snv_table[x,]))
+  for (row_index in snv_table$rowid){
+    snv_table$COSMIC_n_total[row_index] = cosmic_count_results[[row_index]]$COSMIC_n_total
+    snv_table$COSMIC_n_tissue[row_index] = cosmic_count_results[[row_index]]$COSMIC_n_tissue
+  }
+  return(snv_table)
+}
+
+#' Applies counter_cosmic_variant_tissue rowwise to SNV table
+#'
+#' @param table_element Row of SNV table
+#'
+#' @return List of counter_cosmic_variant_tissue results (COSMIC_n_total, COSMIC_n_tissue)
+#' @export
+#'
+#' @examples
+cosmic_counter_per_table_element <- function(table_element){
+  if(is.na(table_element$amino_acid_change) | table_element$amino_acid_change == "p.?"){
+    comic_count = counter_cosmic_variant_tissue(gene = table_element$gene,
+                                                change = table_element$coding,
+                                                sql_con_tbl = CON_TBL)
+  }else{
+    comic_count = counter_cosmic_variant_tissue(gene = table_element$gene,
+                                                change = table_element$one_AA,
+                                                sql_con_tbl = CON_TBL)
+  }
+  return(comic_count)
+}
+
+
