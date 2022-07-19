ac24c5a80284cde52b1cbe62e12380ea6d542213
manzomonza <massimiliano.manzo@posteo.ch>
2022-07-19 14:37:59 +0200

    Change <preuve> to <indication> in CNV annotation

--- R/annot_CNV.R
+++ R/annot_CNV.R
@@ -1,6 +1,6 @@
 #' Annotate CNVs
 #'
-#' @param variant_table 
+#' @param variant_table
 #'
 #' @return
 #' @export
@@ -11,15 +11,15 @@ cnvAnnot <- function(variant_table){
   ## Round copy_number to single digit when 0, otherwise 2-digit
   variant_table <- variant_table %>%
     dplyr::mutate(copy_number = ifelse(copy_number != 0, signif(copy_number, digits = 2), 0 ))
-  
+
   ## Identify if CNV is an amplification or loss
   variant_table <- variant_table %>%
     dplyr::mutate(Diagnose_D = ifelse(fivePercent_conf >= 4, paste0(gene,": ","Hinweis auf eine Amplifikation (", copy_number, "x) auf Chromosom ", chromosome),
                                       ifelse(ninetyfivePercent_conf <= 1, paste0(gene,": ","Hinweis auf einen Verlust (", copy_number, "x) auf Chromosom ", chromosome), NA)),
-                  Diagnose_F = ifelse(fivePercent_conf >= 4, paste0(gene,": ","Preuve d'amplification (", copy_number, "x) sur le chromosome ", chromosome),
+                  Diagnose_F = ifelse(fivePercent_conf >= 4, paste0(gene,": ","indication d'amplification (", copy_number, "x) sur le chromosome ", chromosome),
                                       ifelse(ninetyfivePercent_conf <= 1, paste0(gene,": ","indication d'une perte (", copy_number, "x)  sur le chromosome ", chromosome), NA))
     )
-  
+
   # variant_table <- variant_table %>%
   #   mutate(Link_Methodik = NA,
   #          Allelic_Frequency = NA,
