2ac4499c680f7a20788d40715ec14c7c375c4fb0
manzomonza <massimiliano.manzo@posteo.ch>
2022-07-28 18:06:37 +0200

    Check status

--- R/PathowinAnnotation_WatchdogTables.R
+++ R/PathowinAnnotation_WatchdogTables.R
@@ -39,15 +39,27 @@ annotateWatchdogTables <- function(filepath){
       snv_cancerHotspot = wrapper_table_cancerHotspots(readr::read_tsv(paste0(dir_path, "/prep_snv.txt")))
       snv_cancerHotspot = snv_cancerHotspot %>% dplyr::select(gene, coding, cancerHotspot)
 
+      print('snv')
+      print(snv)
+
       # GENE COLUMN NAME PROBLEM
       if(!'gene' %in% colnames(snv) & 'genes' %in% colnames(snv)){
         snv$gene = snv$genes
       }
+
+      print('tsg')
+      print(snv_tsg)
       #Join TSG
       snv = dplyr::left_join(snv, snv_tsg, by = c("gene", 'coding'))
 
+      print('snv_cosmic')
+      print(snv_cosmic)
       #Join cosmic
       snv = dplyr::left_join(snv, snv_cosmic, by = c("gene", 'coding'))
+
+      print('snv_cancerHotspot')
+      print(snv_cancerHotspot)
+
       #Join cancerhotspot
       snv = dplyr::left_join(snv, snv_cancerHotspot, by = c("gene", 'coding'))
     }
