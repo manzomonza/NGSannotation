82569d19e2bcab5946e98cc14208609591536e4e
manzomonza <massimiliano.manzo@posteo.ch>
2022-08-18 15:28:22 +0200

    Fix count_cosmic_tissue to output snv table

--- R/count_cosmic_tissue.R
+++ R/count_cosmic_tissue.R
@@ -11,16 +11,9 @@
 #' COSMIC_n_tissue breast (43)
 #' COSMIC_n_total 43
 cosmic_sql_search <- function(gene, change, sql_con_tbl){
-  require(magrittr)
-  if(is.na(sql_con_tbl[1])){
-    cosmic_results = list(COSMIC_n_tissue = NA,
-                          COSMIC_n_total = NA)
-    return(cosmic_results)
-  }
+  stopifnot(grepl("^c.", change) | grepl("^p.", change))
+
   gene = paste0("%", gene, "%")
-  #orig_change = change
-  #change = paste0("%", change, "%")
-  ##
   variants_per_tissue = sql_con_tbl %>%
     dplyr::filter(gene_name %LIKE% gene)
 
@@ -34,8 +27,6 @@ cosmic_sql_search <- function(gene, change, sql_con_tbl){
       dplyr::filter(mutation_aa == change) %>%
       dplyr::collect() %>%
       dplyr::distinct()
-  }else{
-    variants_per_tissue = NA
   }
   return(variants_per_tissue)
 }
@@ -48,14 +39,16 @@ cosmic_sql_search <- function(gene, change, sql_con_tbl){
 #'
 #' @examples
 counter_cosmic_variant_tissue <- function(variants_per_tissue){
-  if(is.na(variants_per_tissue)){
+  if(nrow(variants_per_tissue) == 0){
     cosmic_results = list(COSMIC_n_tissue = NA,
                           COSMIC_n_total = NA)
     return(cosmic_results)
   }
   count_variants_per_tissue <- variants_per_tissue %>%
       dplyr::count(primary_site, sort = TRUE)
+
   COSMIC_n_total = nrow(variants_per_tissue)
+
   if(COSMIC_n_total == 0){
     COSMIC_n_tissue = NA
   }else{
@@ -64,6 +57,28 @@ counter_cosmic_variant_tissue <- function(variants_per_tissue){
   cosmic_results = list(COSMIC_n_tissue = COSMIC_n_tissue,
                         COSMIC_n_total = COSMIC_n_total)
   return(cosmic_results)
+}
+
+#' Applies counter_cosmic_variant_tissue rowwise to SNV table
+#'
+#' @param table_element Row of SNV table
+#'
+#' @return List of counter_cosmic_variant_tissue results (COSMIC_n_total, COSMIC_n_tissue)
+#' @export
+#'
+#' @examples
+cosmic_counter_per_table_element <- function(table_element){
+  if(is.na(table_element$amino_acid_change) | table_element$amino_acid_change == "p.?"){
+    var_per_tissue = cosmic_sql_search(gene = table_element$gene,
+                                                change = table_element$coding,
+                                                sql_con_tbl = CON_TBL)
+  }else{
+    var_per_tissue = cosmic_sql_search(gene = table_element$gene,
+                                                change = table_element$one_AA,
+                                                sql_con_tbl = CON_TBL)
+  }
+  count_cosmic_tissue = counter_cosmic_variant_tissue(var_per_tissue)
+  return(count_cosmic_tissue)
 }
 
 #' Wrapper function for COSMIC tissue count
@@ -79,34 +94,16 @@ cosmic_counter_wrapper <- function(snv_table){
   snv_table = snv_table %>% tibble::rowid_to_column()
   snv_table$COSMIC_n_total = NA
   snv_table$COSMIC_n_tissue = NA
-
   cosmic_count_results = lapply(snv_table$rowid, function(x)  cosmic_counter_per_table_element(snv_table[x,]))
 
   for (row_index in snv_table$rowid){
-    snv_table$COSMIC_n_total[row_index] = cosmic_count_results[[row_index]]$COSMIC_n_total
-    snv_table$COSMIC_n_tissue[row_index] = cosmic_count_results[[row_index]]$COSMIC_n_tissue
+    if(is.na(cosmic_count_results[[row_index]]$COSMIC_n_total)){
+      snv_table$COSMIC_n_total[row_index] = NA
+      snv_table$COSMIC_n_tissue[row_index] = NA
+    }else{
+      snv_table$COSMIC_n_total[row_index] = cosmic_count_results[[row_index]]$COSMIC_n_total
+      snv_table$COSMIC_n_tissue[row_index] = cosmic_count_results[[row_index]]$COSMIC_n_tissue
+    }
   }
   return(snv_table)
 }
-
-#' Applies counter_cosmic_variant_tissue rowwise to SNV table
-#'
-#' @param table_element Row of SNV table
-#'
-#' @return List of counter_cosmic_variant_tissue results (COSMIC_n_total, COSMIC_n_tissue)
-#' @export
-#'
-#' @examples
-cosmic_counter_per_table_element <- function(table_element){
-  if(is.na(table_element$amino_acid_change) | table_element$amino_acid_change == "p.?"){
-    cosmic_count = cosmic_sql_search(gene = table_element$gene,
-                                                change = table_element$coding,
-                                                sql_con_tbl = CON_TBL)
-  }else{
-    cosmic_count = cosmic_sql_search(gene = table_element$gene,
-                                                change = table_element$one_AA,
-                                                sql_con_tbl = CON_TBL)
-  }
-  return(cosmic_count)
-}
-
