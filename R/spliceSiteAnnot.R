0dc605a541a485f7df37f435de10d82d87cbf2ab
manzomonza <massimiliano.manzo@posteo.ch>
2023-02-13 20:33:36 +0530

    splicesite rowwise

--- R/spliceSiteAnnot.R
+++ R/spliceSiteAnnot.R
@@ -1,24 +1,31 @@
 
 #' ### splice site annotation Function to interrogate if variant occurs in splice site
 #'
-#' @param variant_table 
+#' @param variant_table
 #'
 #' @return
 #' @export
 #'
 #' @examples
 splicesite_Annot <- function(variant_table){
-  
   location_ls <- variant_table %>% dplyr::select(-contains("rowid")) %>%  tibble::rowid_to_column()
   location_ls <- location_ls %>% dplyr::group_by(rowid) %>% dplyr::group_split()
  # return(location_ls)
-  variant_table <- bind_rows(lapply(location_ls, splicesite_rowwise )) %>% 
+  variant_table <- bind_rows(lapply(location_ls, splicesite_rowwise )) %>%
     dplyr::select(-rowid)
   return(variant_table)
 }
 
-  
-  
+
+
+#' Rowwise splice site annotation
+#'
+#' @param srvt
+#'
+#' @return
+#' @export
+#'
+#' @examples
 splicesite_rowwise <- function(srvt){
   if(is.na(srvt$location)){
     srvt$cds_region = ifelse(is.na(srvt$exon), '', 'exon')
@@ -26,12 +33,11 @@ splicesite_rowwise <- function(srvt){
   }else{
     srvt$cds_region = ifelse(grepl("splice|intronic", srvt$location), "intron", "exon")
     srvt$cds_region_No = as.numeric(ifelse(is.na(srvt$exon), NA,
-                               ifelse(srvt$cds_region == "intron" & grepl("-", srvt$coding), as.numeric(srvt$exon) - 1, as.numeric(srvt$exon) ) ) )
+                               ifelse(srvt$cds_region == "intron" & grepl("-", srvt$coding),
+                                      as.numeric(srvt$exon) - 1, as.numeric(srvt$exon) ) ) )
   }
-  return(srvt) 
+  return(srvt)
 }
 
 
 
-
-  
