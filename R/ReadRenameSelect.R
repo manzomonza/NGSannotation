621ff0d7c158ca807676826cd330ada4059a9b5e
manzomonza <massimiliano.manzo@posteo.ch>
2023-02-17 15:26:57 +0530

    Added gene name sub functionality

--- R/ReadRenameSelect.R
+++ R/ReadRenameSelect.R
@@ -1,4 +1,3 @@
-
 #' read in, rename and select
 #'
 #' @param filepath
@@ -14,6 +13,7 @@ read_rename_select <- function(filepath){
   #print(paste0("Multiple by 100?: ", frequency_col_mult100))
   ir_output <- readIn(filepath) %>%
     renameCol() %>%
+    gene_symbol_sub_table() %>%
     addMissingCols() %>%
     dplyr::select(type, gene, coding, amino_acid_change, percent_frequency, location,
                   transcript, exon, locus, copy_number, cnv_confidence, IR_clinvar) %>%
@@ -24,12 +24,21 @@ read_rename_select <- function(filepath){
 }
 
 
+#' Read rename select for GNXS files
+#'
+#' @param filepath
+#'
+#' @return
+#' @export
+#'
+#' @examples
 read_rename_select_precision_snv <- function(filepath){
   frequency_col_mult100 = readIn(filepath) %>%
     mult100()
   snvindelx <- readIn(filepath) %>%
     addMissingCols() %>%
     renameCol() %>%
+    gene_symbol_sub_table() %>%
     snvParse() %>%
     dplyr::ungroup() %>%
     dplyr::select(-transcript) %>%
---
+++ R/double_gene_name.R
@@ -0,0 +1,36 @@
+# GENE_NAME_LUT = readr::read_csv("./double_gene_name/Double_gene_name_LUT.csv")
+
+#' Substitute comma separated gene symbols with single gene name
+#'
+#' @param gene_symbol
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' LOC101927447,SCUBE1 becomes SCUBE1
+gene_symbol_sub = function(gene_symbol){
+  if(gene_symbol %in% GENE_NAME_LUT$gene){
+    res = subset(GENE_NAME_LUT,  gene == gene_symbol)
+    sub_gene = res$substitute_gene_symbol
+    return(sub_gene)
+  }else{
+    return(gene_symbol)
+  }
+}
+
+
+#' Substitute gene symbols
+#'
+#' @param snvtable
+#'
+#' @return
+#' @export
+#'
+#' @examples
+gene_symbol_sub_table = function(snvtable){
+  snvtable$gene = sapply(snvtable$gene,gene_symbol_sub)
+  return(snvtable)
+}
+
+
