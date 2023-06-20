6a4e5446bf7d4b599060b34aad8e9b7bb9e88382
manzomonza <massimiliano.manzo@posteo.ch>
2023-06-20 09:34:18 +0200

    Variant counts per panel in combined_output file

--- R/count_variants_per_panel.R
+++ R/count_variants_per_panel.R
@@ -1,6 +1,6 @@
 
-#' # COUNT Variants per panel -- only SNVs for now.
-#'
+#' Output counts of variants per panel
+#' only SNVs for now
 #' @param prep_tbl
 #' @param SNV_COUNT
 #'
@@ -8,12 +8,16 @@
 #' @export
 #'
 #' @examples
-count_variants_per_panel = function(prep_tbl, SNV_COUNT){
+count_variants_per_panel = function(prep_tbl, default = SNV_COUNT){
+  prep_tbl = dplyr::select(prep_tbl, gene, coding, one_AA)
   fil_tbl = dplyr::left_join(prep_tbl, SNV_COUNT)
-  fil_tbl$total = sum(fil_tbl$n)
-  fil_tbl$panel_list = paste0(paste0(fil_tbl$workflowName, " (", fil_tbl$n, ")"), collapse = "; ")
-  fil_tbl = dplyr::ungroup(fil_tbl)
-  fil_tbl = dplyr::select(fil_tbl, -workflowName, -n)
+  fil_tbl = dplyr::summarise(fil_tbl, total = sum(n), n = n)
+  fil_tbl = dplyr::arrange(fil_tbl, desc(total))
+  fil_tbl = dplyr::mutate(fil_tbl, panel_list = paste0(workflowName, " (", total, ")", collapse = "; "))
+  fil_tbl = dplyr::group_by(fil_tbl, gene, coding, one_AA, panel_list)
+  fil_tbl = dplyr::summarise(fil_tbl, total = sum(total))
+  fil_tbl = dplyr::select(fil_tbl, gene, coding, one_AA, total, panel_list)
   fil_tbl = dplyr::distinct(fil_tbl)
   return(fil_tbl)
 }
+
